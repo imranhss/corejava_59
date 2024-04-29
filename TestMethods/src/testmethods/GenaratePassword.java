c4e9e5cd87961db5b91cce9c331c8a83a313682b
Muhammad Emran Hossain <emranhss@gmail.com>
2024-04-29 19:01:14 +0600

    add

---
+++ TestMethods/src/testmethods/GenaratePassword.java
@@ -0,0 +1,15 @@
+
+package testmethods;
+
+
+public class GenaratePassword {
+    
+    public static void main(String[] args) {
+     
+        int randomNumber=(int)(Math.rint(3));
+        System.out.print(randomNumber);
+        
+    }
+    
+    
+}
