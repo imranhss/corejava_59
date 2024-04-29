85c51c71d846194f1bcf89ed7136f2eeffd181ee
Muhammad Emran Hossain <emranhss@gmail.com>
2024-04-29 16:49:10 +0600

    add

--- TestMethods/src/testmethods/LoginMethod.java
+++ TestMethods/src/testmethods/LoginMethod.java
@@ -6,31 +6,24 @@ public class LoginMethod {
 
     public static void main(String[] args) {
 
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Pls enter user Name");
-        String userName=s.nextLine();
+        String userName = s.nextLine();
         System.out.println("Pls enter Password");
-        String pass=s.nextLine();
-        
-        boolean result=doLogin(userName, pass);
-        
-        
-        if(result){
-            System.out.println("Welcome "+userName);
-        }else{
-        
-            System.out.println("User name or password is incorrect");        
-        }
-        
-        
+        String pass = s.nextLine();
+
+        doLogin(userName, pass);
+
     }
 
-    public static  boolean doLogin(String userName, String password) {
+    public static void doLogin(String userName, String password) {
 
         if (userName.equalsIgnoreCase("java") && password.equals("java1234")) {
-            return true;
+            System.out.println("Welcome " + userName);
+
         } else {
-               return false;
+            System.out.println("User name or password is incorrect");
+
         }
 
     }
