c9c13269398cde60c99b1f4471352905d759f8ff
Muhammad Emran Hossain <emranhss@gmail.com>
2024-05-02 18:27:48 +0600

    add

--- TestTwoDinmensionalArray59/src/testtwodinmensionalarray59/TestTwoDinmensionalArray59.java
+++ TestTwoDinmensionalArray59/src/testtwodinmensionalarray59/TestTwoDinmensionalArray59.java
@@ -1,29 +1,38 @@
-
 package testtwodinmensionalarray59;
 
 import java.util.Arrays;
 import java.util.Random;
 
-
 public class TestTwoDinmensionalArray59 {
 
     public static void main(String[] args) {
+
+//        int[][] numers=new int[3][4];
+//        
+//        Random random=new Random();
+//        
+//        for(int row=0; row<numers.length; row++){
+//            for(int column=0; column<numers[0].length; column++){
+//                int randomNumber=random.nextInt(20);
+//                numers[row][column]=randomNumber;
+//            }        
+//        }
+        int[][] numers = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int sum=doSumOfArrays(numers);
         
-        int[][] numers=new int[3][4];
-        
-        Random random=new Random();
-        
-        for(int row=0; row<numers.length; row++){
-            for(int column=0; column<numers[1].length; column++){
-                int randomNumber=random.nextInt(20);
-                numers[row][column]=randomNumber;
-            }        
-        }
-        
-        
-        
-        System.out.println(Arrays.deepToString(numers));
-        
+        System.out.println(sum);
+
     }
-    
+
+    public static int doSumOfArrays(int[][] myArrays) {
+        int sum = 0;
+        for (int row = 0; row < myArrays.length; row++) {
+            for (int col = 0; col < myArrays[0].length; col++) {
+                sum += myArrays[row][col];
+            }
+        }
+        return sum;
+    }
+
 }
