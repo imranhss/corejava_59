d276281a5d4362d4db3aeee82a110c5ef3cc58db
Muhammad Emran Hossain <emranhss@gmail.com>
2024-05-05 16:25:14 +0600

    add

---
+++ TestClassAndObject59/src/testclassandobject59/TestClassAndObject59.java
@@ -0,0 +1,18 @@
+package testclassandobject59;
+
+import geomatry.Circle;
+
+public class TestClassAndObject59 {
+    
+    public static void main(String[] args) {
+        
+        Circle circle = new Circle(5);
+        
+        //circle.setRadious(3);
+        
+        System.out.println(circle.getArea());
+        System.out.println(circle.getPerimeter());
+        
+    }
+    
+}
