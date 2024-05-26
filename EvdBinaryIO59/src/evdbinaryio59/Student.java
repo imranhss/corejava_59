34df11f104950a9a327761d2eddd5cc9e2250a0c
Muhammad Emran Hossain <emranhss@gmail.com>
2024-05-26 17:59:40 +0600

    add

---
+++ EvdBinaryIO59/src/evdbinaryio59/Student.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package evdbinaryio59;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author user
+ */
+public class Student implements Serializable{
+    
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", age=" + age + '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+    
+    
+}
